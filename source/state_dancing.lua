3e310cd36b5dd12640457ef0866e7d252634c41d
Ryan Loader <talk@ryanloader.me>
2025-01-25 14:11:59 +0000

    Add floating bubble to dancing state

--- source/state_dancing.lua
+++ source/state_dancing.lua
@@ -17,6 +17,11 @@ function DancingState:init()
 
     self.dancer2 = AnimatedSprite.new(dancerImageTable)
     self.dancer2:addState('idle', 1, 3, { tickStep = 4 })
+
+    self.bubbleRadius = 24
+    self.bubbleStrafeSpeed = 4
+    self.bubblePositionX = 228
+    self.bubblePositionY = 240 + self.bubbleRadius
 end
 
 function DancingState:enter()
@@ -38,5 +43,34 @@ function DancingState:update()
     if playdate.buttonJustPressed(playdate.kButtonB) then
         return BlowBubblesState()
     end
+
+    if playdate.buttonJustPressed(playdate.kButtonA) then
+        -- TODO: Check if you score points
+        return BlowBubblesState()
+    end
+
+    self.bubblePositionY -= 1
+    local bubbleHasLeftScene = self.bubblePositionY < -self.bubbleRadius
+        or self.bubblePositionX < -self.bubbleRadius
+        or self.bubblePositionX > 400 + self.bubbleRadius
+    if bubbleHasLeftScene then
+        return BlowBubblesState()
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+        self.bubblePositionX -= self.bubbleStrafeSpeed
+    end
+    if playdate.buttonIsPressed(playdate.kButtonRight) then
+        self.bubblePositionX += self.bubbleStrafeSpeed
+    end
+
     return self
+end
+
+function DancingState:draw()
+    gfx.pushContext()
+    gfx.setLineWidth(1)
+    gfx.setColor(gfx.kColorBlack)
+    gfx.drawCircleAtPoint(self.bubblePositionX, self.bubblePositionY, self.bubbleRadius)
+    gfx.popContext()
 end
